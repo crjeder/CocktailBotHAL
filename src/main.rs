5495a11d19e1f43c1db8751b29e76f71688df696
crjeder <crjeder@gmail.com>
2019-10-10 15:24:45 +0200

    added hectors api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
 fn main() {
     println!("Hello, world!");
 }
+// config hx711
+// self.hx.reset()
+// self.hx.tare()
+
+// config servos
+// self.numValves = len(self.valveChannels)
+           
+// config arm     
+// self.arm = cfg["arm"]["SENSE"]
+
+// config pump
+//self.pump = cfg["pump"]["MOTOR"]
+
+// light
+// def light_on(self):
+// def light_off(self):
+  
+// arm
+// def arm_out(self):
+// def arm_in(self):
+// def arm_pos(self):
+
+// scale
+// def scale_readout(self):
+// def scale_tare(self):
+
+// pump
+// def pump_start(self):
+// def pump_stop(self):
+
+// valve
+// def valve_open(self, index, open=1):
+// def valve_close(self, index):
+// def valve_dose(self, index, amount, timeout=30):
+
+// finger
+// config
+// def finger(self, pos=0):
+// def ping(self, num, retract=True):
+
+// cleaning
+// def cleanAndExit(self):
