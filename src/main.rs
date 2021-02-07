f505a6aa5df7aa8175f54a066b93b364911fa855
Christoph <crjeder@gmail.com>
2021-02-07 11:33:09 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main()
         name: String::from("Screwdriver"),
         glass: String::from("Highball"),
         category: String::from("All Day"),
+		stir: true,
         ingredients: vec!
         [
           Ingredient {amount: 20, name: String::from("Vodka")},
@@ -12,6 +13,24 @@ fn main()
         ],
         garnish: String::from("Slice of Orange"),
         preparation: String::from("Pour Vodka and Orange Juice over ice")
+    };
+	
+	let sunrise = Cocktail
+    {
+        name: String::from("Tequila Sunrise"),
+        glass: String::from("Huricane"),
+        category: String::from("Classic"),
+		stir: false,
+        ingredients: vec!
+        [
+          Ingredient {amount: 10, name: String::from("Grenadine")},
+		  Ingredient {amount: 60, name: String::from("Tequila")},
+          Ingredient {amount: 15, name: String::from("Tripple Sec")},
+		  Ingredient {amount: 75, name: String::from("Orange Juice")},
+		  Ingredient {amount: 20, name: String::from("Lime Juice")}
+        ],
+        garnish: String::from("Slice of orange and cherry"),
+        preparation: String::from("Pour Grenadine over ice and then add the other ingredient slowly. Do not stir!")
     };
 
     println!("Your Cocktail Choice is:");
@@ -25,63 +44,99 @@ fn main()
     println!("Garnish with {}", screwdriver.garnish);
     println!("Enjoy!");
 
-    let config = Config {dispensers: 16, display: false};
-    let dispenser_config = vec!
-    [
-        Liquid {name: String::from("Vodka"), density: 94, alcohol: 40, suggar: 0},
-        Liquid {name: String::from("Gin"), density: 94, alcohol: 40, suggar: 0},
-        Liquid {name: String::from("White Rum"), density: 94, alcohol: 40, suggar: 0},
-        Liquid {name: String::from("Tequila"), density: 94, alcohol: 40, suggar: 0},
-        Liquid {name: String::from("Tripple Sec"), density: 94, alcohol: 40, suggar: 10},
-        Liquid {name: String::from("Orange Juice"), density: 104, alcohol: 0, suggar: 8},
-        Liquid {name: String::from("Ginger Ale"), density: 104, alcohol: 40, suggar: 0},
-        Liquid {name: String::from("Bitter Lemon"), density: 104, alcohol: 40, suggar: 0},
-        Liquid {name: String::from("Lime Juice"), density: 104, alcohol: 40, suggar: 0},
-        Liquid {name: String::from("Coke"), density: 104, alcohol: 0, suggar: 0},
-        Liquid {name: String::from("Prosecco"), density: 94, alcohol: 40, suggar: 0},
-        Liquid {name: String::from("Grenadine"), density: 121, alcohol: 0, suggar: 47},
-        Liquid {name: String::from("Aperol"), density: 100, alcohol: 40, suggar: 47},
-        Liquid {name: String::from("Syrup"), density: 121, alcohol: 0, suggar: 60},
-        Liquid {name: String::from("Amaretto"), density: 100, alcohol: 40, suggar: 47}
-    ];
+	let bot = Cocktailbot
+	{
+		config = Config {dispensers: 16, display: false};
+		dispenser_config = vec!
+		[
+			Liquid {name: String::from("Vodka"), density: 94, alcohol: 40, suggar: 0},
+			Liquid {name: String::from("Gin"), density: 94, alcohol: 40, suggar: 0},
+			Liquid {name: String::from("White Rum"), density: 94, alcohol: 40, suggar: 0},
+			Liquid {name: String::from("Tequila"), density: 94, alcohol: 40, suggar: 0},
+			Liquid {name: String::from("Tripple Sec"), density: 94, alcohol: 40, suggar: 10},
+			Liquid {name: String::from("Orange Juice"), density: 104, alcohol: 0, suggar: 8},
+			Liquid {name: String::from("Ginger Ale"), density: 104, alcohol: 40, suggar: 0},
+			Liquid {name: String::from("Tonic"), density: 104, alcohol: 40, suggar: 8},
+			Liquid {name: String::from("Lime Juice"), density: 104, alcohol: 40, suggar: 0},
+			Liquid {name: String::from("Coke"), density: 104, alcohol: 0, suggar: 9},
+			Liquid {name: String::from("Prosecco"), density: 94, alcohol: 40, suggar: 0},
+			Liquid {name: String::from("Grenadine"), density: 121, alcohol: 0, suggar: 47},
+			Liquid {name: String::from("Aperol"), density: 100, alcohol: 40, suggar: 47},
+			Liquid {name: String::from("Syrup"), density: 129, alcohol: 0, suggar: 60},
+			Liquid {name: String::from("Amaretto"), density: 100, alcohol: 40, suggar: 47}
+		],
+		glasses = vec!
+		[
+			Glass {name: String::from("Highball"), volume: 100, weight: 80},
+			Glass {name: String::from("Tumbler"), volume: 100, weight: 80}
+		],
+	}
 
-    let glasses = vec!
-    [
-        Glass {name: String::from("Highball"), volume: 100, weight: 80},
-        Glass {name: String::from("Tumbler"), volume: 100, weight: 80}
-    ];
 }
 
-struct Ingredient
+struct Dispenser
+{
+	liquids: vec<Liquid>,
+	max_liquids: u8,
+}	
+	
+impl Dispenser
+{
+	dispense(nr_liquid: u8, amount: u8)
+}
+
+pub struct Cocktailbot
+{
+	pub config: Config,
+	Dispenser: dispenser_config: vec<Liquid>,
+	pub glasses: Vec<Glass>,
+	cocktails_mixed: u8,
+	dispenser: Dispenser
+}
+
+impl Cocktailbot
+{
+	pub fn mix(&mut self, cocktail: Cocktail)
+	{
+		if self.config.display display(cocktail.name);
+		for ingredient in cocktail.ingredients
+		{
+			self.dispenser.dispense();
+		self.cocktails_mixed = self.cocktails_mixed + 1;
+	}
+}
+
+pub struct Ingredient
 {
     amount: u8,                 // in ml
     name: String
 }
 
-struct Cocktail
+pub struct Cocktail
 {
     name: String,
     glass: String,
     category: String,
     ingredients: Vec<Ingredient>,
     garnish: String,
+	stir: bool,
     preparation: String
 }
 
-struct Config
+pub struct Config
 {
     dispensers: u8,
     display: bool
 }
 
-struct Glass
+pub struct Glass
 {
     name: String,
     volume: u8,                 // in ml
     weight: u8                  // in g
 }
 
-struct Liquid
+pub struct Liquid
 {
     name: String,
     density: u8,                // in g / cm3 * 100
