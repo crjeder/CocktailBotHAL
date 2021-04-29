2c18f2281dc5933907ae3812d7e617dee410519f
crjeder <crjeder@gmail.com>
2021-04-29 13:01:30 +0200

    added error type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 fn main()
 {
     let screwdriver = Cocktail
@@ -14,7 +16,7 @@ fn main()
         garnish: String::from("Slice of Orange"),
         preparation: String::from("Pour Vodka and Orange Juice over ice")
     };
-	
+
 	let sunrise = Cocktail
     {
         name: String::from("Tequila Sunrise"),
@@ -46,49 +48,60 @@ fn main()
 
 	let bot = Cocktailbot
 	{
-		config = Config {dispensers: 16, display: false};
-		dispenser_config = vec!
-		[
-			Liquid {name: String::from("Vodka"), density: 94, alcohol: 40, suggar: 0},
-			Liquid {name: String::from("Gin"), density: 94, alcohol: 40, suggar: 0},
-			Liquid {name: String::from("White Rum"), density: 94, alcohol: 40, suggar: 0},
-			Liquid {name: String::from("Tequila"), density: 94, alcohol: 40, suggar: 0},
-			Liquid {name: String::from("Tripple Sec"), density: 94, alcohol: 40, suggar: 10},
-			Liquid {name: String::from("Orange Juice"), density: 104, alcohol: 0, suggar: 8},
-			Liquid {name: String::from("Ginger Ale"), density: 104, alcohol: 40, suggar: 0},
-			Liquid {name: String::from("Tonic"), density: 104, alcohol: 40, suggar: 8},
-			Liquid {name: String::from("Lime Juice"), density: 104, alcohol: 40, suggar: 0},
-			Liquid {name: String::from("Coke"), density: 104, alcohol: 0, suggar: 9},
-			Liquid {name: String::from("Prosecco"), density: 94, alcohol: 40, suggar: 0},
-			Liquid {name: String::from("Grenadine"), density: 121, alcohol: 0, suggar: 47},
-			Liquid {name: String::from("Aperol"), density: 100, alcohol: 40, suggar: 47},
-			Liquid {name: String::from("Syrup"), density: 129, alcohol: 0, suggar: 60},
-			Liquid {name: String::from("Amaretto"), density: 100, alcohol: 40, suggar: 47}
-		],
-		glasses = vec!
+		config: Config {display: false},
+		dispenser: Dispenser
+        {
+            max_liquids: 16,
+            liquids: vec!
+		    [
+                Liquid {name: String::from("Vodka"), density: 94, alcohol: 40, suggar: 0},
+                Liquid {name: String::from("Gin"), density: 94, alcohol: 40, suggar: 0},
+			    Liquid {name: String::from("White Rum"), density: 94, alcohol: 40, suggar: 0},
+			    Liquid {name: String::from("Tequila"), density: 94, alcohol: 40, suggar: 0},
+			    Liquid {name: String::from("Tripple Sec"), density: 94, alcohol: 40, suggar: 10},
+                Liquid {name: String::from("Orange Juice"), density: 104, alcohol: 0, suggar: 8},
+                Liquid {name: String::from("Ginger Ale"), density: 104, alcohol: 40, suggar: 0},
+			    Liquid {name: String::from("Tonic"), density: 104, alcohol: 40, suggar: 8},
+			    Liquid {name: String::from("Lime Juice"), density: 104, alcohol: 40, suggar: 0},
+			    Liquid {name: String::from("Coke"), density: 104, alcohol: 0, suggar: 9},
+			    Liquid {name: String::from("Prosecco"), density: 94, alcohol: 40, suggar: 0},
+			    Liquid {name: String::from("Grenadine"), density: 121, alcohol: 0, suggar: 47},
+			    Liquid {name: String::from("Aperol"), density: 100, alcohol: 40, suggar: 47},
+			    Liquid {name: String::from("Syrup"), density: 129, alcohol: 0, suggar: 60},
+			    Liquid {name: String::from("Amaretto"), density: 100, alcohol: 40, suggar: 47}
+		    ]
+        },
+		glasses: vec!
 		[
 			Glass {name: String::from("Highball"), volume: 100, weight: 80},
-			Glass {name: String::from("Tumbler"), volume: 100, weight: 80}
+			Glass {name: String::from("Tumbler"), volume: 100, weight: 50}
 		],
-	}
+        cocktails_mixed: 0
+	};
 
 }
 
 struct Dispenser
 {
-	liquids: vec<Liquid>,
-	max_liquids: u8,
-}	
-	
+	pub liquids: Vec<Liquid>,
+	max_liquids: u8
+}
+
 impl Dispenser
 {
-	dispense(nr_liquid: u8, amount: u8)
+	pub fn dispense(nr_liquid: u8, amount: u8) -> Result<u8, BarBotError>
+    {
+        Ok(amount)
+        // BarBotError
+        //  OutOfLiquid
+        //  Spill
+        //  :
+    }
 }
 
 pub struct Cocktailbot
 {
 	pub config: Config,
-	Dispenser: dispenser_config: vec<Liquid>,
 	pub glasses: Vec<Glass>,
 	cocktails_mixed: u8,
 	dispenser: Dispenser
@@ -98,12 +111,18 @@ impl Cocktailbot
 {
 	pub fn mix(&mut self, cocktail: Cocktail)
 	{
-		if self.config.display display(cocktail.name);
+		if self.config.display
+        {
+            // display(cocktail.name);
+        }
 		for ingredient in cocktail.ingredients
 		{
-			self.dispenser.dispense();
-		self.cocktails_mixed = self.cocktails_mixed + 1;
+            // nr = find_dispenser(ingredient);
+			// self.dispenser.dispense(nr, );
+        }
+		self.cocktails_mixed += 1;        // record keeping
 	}
+    // fn display()
 }
 
 pub struct Ingredient
@@ -125,7 +144,6 @@ pub struct Cocktail
 
 pub struct Config
 {
-    dispensers: u8,
     display: bool
 }
 
@@ -142,4 +160,22 @@ pub struct Liquid
     density: u8,                // in g / cm3 * 100
     alcohol: u8,                // in vol % (ABV)
     suggar: u8,                 // in g / 100 g
+}
+
+pub struct BarBotError;
+
+impl fmt::Debug for BarBotError
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "{{Generic BarBotError in file: {}, line: {} }}", file!(), line!())
+    }
+}
+
+impl fmt::Display for BarBotError
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
+    {
+        write!(f, "Generic BarBotError")
+    }
 }
